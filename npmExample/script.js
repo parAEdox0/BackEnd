e10e0a8a22cdc826e43b775d1de39c4e66d4c54d
Parth Ekbote <paraedox0@gmail.com>
2024-03-19 00:43:06 +0530

    Example of installing and using a package using npm

---
+++ npmExample/script.js
@@ -0,0 +1,15 @@
+//In this we can learn the installation and implementation of any node package using npm
+//head to npmjs.com and search for package you would like to install
+//usage guide is give on the site
+//In this case we are installing figlet
+
+var figlet = require("figlet");
+
+figlet("HAHAHA", function (err, data) {
+    if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
