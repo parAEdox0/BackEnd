87b3c8f93a3abf713ea1c67724841477f2737684
Parth Ekbote <paraedox0@gmail.com>
2024-03-19 19:26:43 +0530

    static files and error handling in express js

--- ejsExample/script.js
+++ ejsExample/script.js
@@ -4,6 +4,7 @@ const app = express()
 
 //configuring ejs
 app.set("view engine", "ejs");
+app.use(express.static("./public"))
 
 app.get('/', function (req, res) {
     res.render("index", { name: "parth" })
@@ -11,6 +12,18 @@ app.get('/', function (req, res) {
 
 app.get('/contactus', function (req, res) {
     res.render("contactUs")
+})
+
+app.get('/404error', function (req, res, next) {
+    throw Error("Error 404")
+})
+
+app.use(function errorHandler(err, req, res, next) {
+    if (res.headersSent) {
+        return next(err)
+    }
+    res.status(500)
+    res.render('error', { error: err })
 })
 
 app.listen(2080)
